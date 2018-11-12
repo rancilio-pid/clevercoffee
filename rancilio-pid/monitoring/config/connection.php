3f085bcaf8219dfff953cd524b46c9c8487a3de0
Markus Brunner <info@brunner-it.de>
2018-11-12 09:13:30 +0100

    Add Monitoring WebPortal
    http://monitoring.rancilio-pid.de/status.php?node=markus&history=30

---
+++ rancilio-pid/monitoring/config/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+//database
+
+define('DB_HOST', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+/*
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'freifunk');
+*/
+
+
+
+?>
