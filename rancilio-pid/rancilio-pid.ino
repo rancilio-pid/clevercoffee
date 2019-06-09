c230ce08c4f936274c764aa7278f0139c0e055f6
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2019-06-09 19:56:12 +0200

    Update rancilio-pid.ino

--- rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid.ino
@@ -487,7 +487,7 @@ void setup() {
     //display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
     display.clearDisplay();
   }
-  displaymessage("Version 1.7.5 MASTER","07.06.2019", Display);
+  displaymessage("Version 1.8.0 BETA","09.06.2019", Display);
   delay(2000);
 
   /********************************************************
