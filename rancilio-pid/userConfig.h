2d7c52493664d140d6ffdde9544789b101529cba
Miau09 <50794912+Miau09@users.noreply.github.com>
2019-06-13 20:29:36 +0200

    Update userConfig.h
    fix copy&paste error

--- rancilio-pid/userConfig.h
+++ rancilio-pid/userConfig.h
@@ -20,9 +20,9 @@
 #define PONE 1               // 1 = P_ON_E, 0 = P_ON_M
 
 // Wifi
-char auth[] = "blynkauthcode";
-char ssid[] = "wlanname";
-char pass[] = "wlanpass";
+#define AUTH "blynkauthcode";
+#define D_SSID "wlanname";
+#define PASS "wlanpass";
 
 // OTA
 #define OTAHOST "Rancilio"   // Name to be shown in ARUDINO IDE Port
