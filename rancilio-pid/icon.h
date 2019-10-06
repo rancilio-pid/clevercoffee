cb631e026047f884cef5b391245c446b17266ff0
Miau09 <50794912+Miau09@users.noreply.github.com>
2019-10-06 15:26:40 +0200

    Update icon.h
    added icons

--- rancilio-pid/icon.h
+++ rancilio-pid/icon.h
@@ -1,3 +1,7 @@
+/********************************************************
+  Version 1.1 (06.10.2019)
+******************************************************/
+
 #define logo_width 23
 #define logo_height 58
 #define startLogo_width 45
@@ -32,4 +36,44 @@ static const unsigned char PROGMEM logo_bits[] = {
 	0xfc, 0x00, 0x3f, 0x80, 0x01, 0xff, 0xf8, 0x00, 0x1f, 0x80, 0x00, 0x7f, 0xe0, 0x00, 0x0f, 0x80, 
 	0x00, 0x1f, 0x80, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+static const unsigned char PROGMEM antenna_OK[] = {
+  B11111000,
+  B10101000,
+  B10101000,
+  B01110000,
+  B00100000,
+  B00100000,
+  B00100000,
+  B00100000
+};
+static const unsigned char PROGMEM antenna_NOK[] = {
+  B10111100,
+  B01000100,
+  B00100100,
+  B00010000,
+  B00001000,
+  B00010100,
+  B00010010,
+  B00010001
+};
+static const unsigned char PROGMEM blynk_OK[] = {
+  B00000000, B00000000,
+  B10011110, B00100000,
+  B01010001, B01000000,
+  B00010001, B00000000,
+  B11011110, B01100000,
+  B00010001, B00000000,
+  B01010001, B01000000,
+  B10011110, B00100000
+};
+static const unsigned char PROGMEM blynk_NOK[] = {
+  B10000000,
+  B01011100,
+  B00100010,
+  B00010010,
+  B00101000,
+  B00100100,
+  B00100010,
+  B00111101
 };
