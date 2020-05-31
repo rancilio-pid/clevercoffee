bc30601656439a7149cbae387ab87bc9da8e6d77
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2020-05-31 15:39:01 +0200

    Update userConfig.h

--- rancilio-pid/userConfig.h
+++ rancilio-pid/userConfig.h
@@ -11,7 +11,7 @@
 ******************************************************/
 #define DISPLAY 2            // 0 = deactivated, 1 = SH1106 (e.g. 1.3 "128x64), 2 = SSD1306 (e.g. 0.96" 128x64)
 #define OFFLINEMODUS 0       // 0 = Blynk and WIFI are used; 1 = offline mode (only preconfigured values in code are used!)
-#define ONLYPID 0            // 1 = Only PID, no preinfusion; 0 = PID and preinfusion
+#define ONLYPID 1            // 1 = Only PID, no preinfusion; 0 = PID and preinfusion
 #define TEMPSENSOR 2         // 2 = TSIC306
 #define BREWDETECTION 1      // 0 = off; 1 = Software; 2 = Hardware
 #define FALLBACK 1           // 1 = fallback to values stored in eeprom, if blynk is not working; 0 = deactivated
