18e18c0ba54c6180138328fabeabe1c2c363d1ab
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2021-03-06 13:10:06 +0100

    Update userConfig.h

--- rancilio-pid/userConfig.h
+++ rancilio-pid/userConfig.h
@@ -28,7 +28,7 @@
 
 // PID & Hardware
 #define ONLYPID 1                  // 1 = Only PID, 0 = PID and preinfusion
-#define BREWDETECTION 1            // 0 = off, 1 = Software (Onlypid 0), 2 = Hardware (Onlypid 0), 3 = Sensor/Hardware for Only PID 
+#define BREWDETECTION 1            // 0 = off, 1 = Software (Onlypid 1), 2 = Hardware (Onlypid 0), 3 = Sensor/Hardware for Only PID 
 #define COLDSTART_PID 1            // 1 = default coldstart values, 2 = custom values via blynk (expert mode activated) 
 #define TRIGGERTYPE HIGH           // LOW = low trigger, HIGH = high trigger relay
 // BREWDETECTION 3 configuration
