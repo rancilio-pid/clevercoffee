e98afb60d379d543ddd6cfe71b7036ac8cd04d0e
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2021-04-05 11:34:46 +0200

    blinking onlytempdisplay

--- rancilio-pid/Displaytemplatetemponly.h
+++ rancilio-pid/Displaytemplatetemponly.h
@@ -1,6 +1,10 @@
 /********************************************************
     send data to display
 ******************************************************/
+// Define some Displayoptions
+int blinkingtemp = 1  ;         // 0: blinking near setpoint, 1: blinking by heating far away from setpoint
+float blinkingtempoffset = 0.3; // offset for blinking
+
 void printScreen() 
 {
 
@@ -12,59 +16,73 @@ void printScreen()
    ((machinestate == 10)  && HEATINGLOGO == 0) ||
    ((machinestate == 90)  && OFFLINEGLOGO == 0)  
   ) 
-   {
-   
-    if (!sensorError) {
-      u8g2.clearBuffer();
-      //draw outline frame
-      //u8g2.drawFrame(0, 0, 128, 64);
-     
-
-      //draw (blinking) temp
-    if (fabs(Input - setPoint) < 0.3) {
-    if (isrCounter < 500) {
-      if (Input < 99.999) {
-        u8g2.setCursor(13, 12);
-        u8g2.setFont(u8g2_font_fub35_tf);
-        u8g2.print(Input,1);
-      }
-      else {
-        u8g2.setCursor(-1, 12);
-        u8g2.setFont(u8g2_font_fub35_tf);
-        u8g2.print(Input,1);
-      }
-    }
-  } else {
-    if (Input < 99.999) {
-      u8g2.setCursor(13, 12);
-      u8g2.setFont(u8g2_font_fub35_tf);
-      u8g2.print(Input, 1);
-    }
-    else {
-      u8g2.setCursor(-1, 12);
-      u8g2.setFont(u8g2_font_fub35_tf);
-      u8g2.print(Input, 1);
-    }
-  }
-}
-
-      // Für Statusinfos
-      if (Offlinemodus == 0) {
-        getSignalStrength();
-        if (WiFi.status() != WL_CONNECTED) {
-          u8g2.drawFrame(116, 28, 12, 12);
-          u8g2.drawXBMP(118, 30, 8, 8, antenna_NOK_u8g2);
-        } else {
-          if (!Blynk.connected()) {
-            u8g2.drawFrame(116, 28, 12, 12);
-            u8g2.drawXBMP(118, 30, 8, 8, blynk_NOK_u8g2);
+  {
+    if (!sensorError) 
+    {
+        u8g2.clearBuffer();
+        //draw outline frame
+        //u8g2.drawFrame(0, 0, 128, 64);
+        //draw (blinking) temp
+      if 
+      ( 
+          (fabs(Input - setPoint) < blinkingtempoffset && blinkingtemp == 0)  ||
+          (fabs(Input - setPoint) >= blinkingtempoffset && blinkingtemp == 1)
+      ) 
+      {
+        if (isrCounter < 500) 
+        {
+          if (Input < 99.999) 
+          {
+            u8g2.setCursor(13, 12);
+            u8g2.setFont(u8g2_font_fub35_tf);
+            u8g2.print(Input,1);
+          }
+          else 
+          {
+            u8g2.setCursor(-1, 12);
+            u8g2.setFont(u8g2_font_fub35_tf);
+            u8g2.print(Input,1);
           }
         }
-      } else {
-        u8g2.drawFrame(116, 28, 12, 12);
-        u8g2.setCursor(120, 30);
-        u8g2.print("O");
+      } else 
+      {
+        if (Input < 99.999) 
+        {
+          u8g2.setCursor(13, 12);
+          u8g2.setFont(u8g2_font_fub35_tf);
+          u8g2.print(Input, 1);
+        }
+        else 
+        {
+          u8g2.setCursor(-1, 12);
+          u8g2.setFont(u8g2_font_fub35_tf);
+          u8g2.print(Input, 1);
+        }
       }
-      u8g2.sendBuffer();
     }
+
+  // Für Statusinfos
+  if (Offlinemodus == 0) 
+  {
+    getSignalStrength();
+    if (WiFi.status() != WL_CONNECTED) 
+    {
+      u8g2.drawFrame(116, 28, 12, 12);
+      u8g2.drawXBMP(118, 30, 8, 8, antenna_NOK_u8g2);
+    } else 
+    {
+      if (!Blynk.connected()) 
+      {
+        u8g2.drawFrame(116, 28, 12, 12);
+        u8g2.drawXBMP(118, 30, 8, 8, blynk_NOK_u8g2);
+      }
+    }
+  } else 
+  {
+    u8g2.drawFrame(116, 28, 12, 12);
+    u8g2.setCursor(120, 30);
+    u8g2.print("O");
   }
+  u8g2.sendBuffer();
+  }
+}
