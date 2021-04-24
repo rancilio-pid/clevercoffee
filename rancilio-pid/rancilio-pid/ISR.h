6ec1c02ac76dbf02b36b408b6559c9e07d618189
Adrian Immer <62163284+lebuni@users.noreply.github.com>
2021-04-24 17:43:29 +0200

    Move bPID.Compute() from ISR to loop()

--- rancilio-pid/rancilio-pid/ISR.h
+++ rancilio-pid/rancilio-pid/ISR.h
@@ -1,54 +1,38 @@
 /********************************************************
-    Timer 1 - ISR for PID calculation and heat realay output
+Timer 1 - ISR for PID calculation and heat realay output
 ******************************************************/
 
+#ifdef ESP8266
+	void ICACHE_RAM_ATTR onTimer1ISR() {
+		timer1_write(6250); // set interrupt time to 20ms
 
+		if (Output <= isrCounter) {
+			digitalWrite(pinRelayHeater, LOW);
+		} else {
+			digitalWrite(pinRelayHeater, HIGH);
+		}
 
+		isrCounter += 20; // += 20 because one tick = 20ms
 
+		//set PID output as relay command
+		if (isrCounter > windowSize) {
+			isrCounter = 0;
+		}
+	}
 
+#elif defined(ESP32)
+	void IRAM_ATTR onTimer() {
+		if (Output <= isrCounter) {
+			digitalWrite(pinRelayHeater, LOW);
+		} else {
+			digitalWrite(pinRelayHeater, HIGH);
+		}
 
+		isrCounter += 10; // += 10 because one tick = 10ms
 
-#if defined(ESP8266) // ESP8266
-    
-    void ICACHE_RAM_ATTR onTimer1ISR() {
-    timer1_write(6250); // set interrupt time to 20ms
-
-    if (Output <= isrCounter) {
-        digitalWrite(pinRelayHeater, LOW);
-    } else {
-        digitalWrite(pinRelayHeater, HIGH);
-    }
-
-    isrCounter += 20; // += 20 because one tick = 20ms
-    //set PID output as relais commands
-    if (isrCounter > windowSize) {
-        isrCounter = 0;
-    }
-
-    //run PID calculation
-    bPID.Compute();
-    }
+		//set PID output as relay command
+		if (isrCounter > windowSize) {
+			isrCounter = 0;
+		}
+	}
 #endif
-
-#if defined(ESP32) // ESP32
-  void IRAM_ATTR onTimer(){
-    
-    //timer1_write(50000); // set interrupt time to 10ms
-      timerAlarmWrite(timer, 10000, true);
-    if (Output <= isrCounter) {
-      digitalWrite(pinRelayHeater, LOW);
-    } else {
-      digitalWrite(pinRelayHeater, HIGH);
-    }
-  
-    isrCounter += 10; // += 10 because one tick = 10ms
-    //set PID output as relais commands
-    if (isrCounter > windowSize) {
-      isrCounter = 0;
-    }
-  
-    //run PID calculation
-    bPID.Compute();
-  }
-
- #endif   
