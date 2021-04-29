d83609c61dd5747a79003fc802d25ffabc1e8c59
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2021-04-29 20:30:27 +0200

    version

--- rancilio-pid/rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid/rancilio-pid.ino
@@ -1,5 +1,5 @@
 /********************************************************
-   Version 2.9.0 (02.04.2021)  
+   Version 2.9.1 (29.04.2021)  
 ******************************************************/
 
 /********************************************************
@@ -143,7 +143,7 @@ int relayON, relayOFF;          // used for relay trigger type. Do not change!
 boolean kaltstart = true;       // true = Rancilio started for first time
 boolean emergencyStop = false;  // Notstop bei zu hoher Temperatur
 double EmergencyStopTemp = 120; // Temp EmergencyStopTemp
-const char* sysVersion PROGMEM  = "Version 2.9.0 MASTER";   //System version
+const char* sysVersion PROGMEM  = "Version 2.9.1 MASTER";   //System version
 int inX = 0, inY = 0, inOld = 0, inSum = 0; //used for filter()
 int bars = 0; //used for getSignalStrength()
 boolean brewDetected = 0;
