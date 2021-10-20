3d52fb153f6534da54d6bab327c845f8e9a4230a
Daniel Wagner <dtw@dropbox.com>
2021-10-19 23:58:27 -0400

    it's not pretty but it works

---
+++ rancilio-pid/rancilio-pid/RancilioServer.h
@@ -0,0 +1,31 @@
+#ifndef rancilioserver_h
+#define rancilioserver_h
+
+#include <Arduino.h>
+
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+enum EditableKind {
+    kInteger,
+    kDouble,
+};
+
+struct editable_t {
+    String displayName;
+    EditableKind type;
+    void *ptr;  // TODO: there must be a tidier way to do this? could we use c++ templates?
+};
+
+void serverSetup();
+
+// We define these in the ino file
+extern std::vector<editable_t> editableVars;
+
+#endif
--- rancilio-pid/rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid/rancilio-pid.ino
@@ -307,6 +307,20 @@ unsigned long previousMillisBlynk;  // initialisation at the end of init()
 const unsigned long intervalBlynk = 1000;
 int blynksendcounter = 1;
 
+/********************************************************
+   HTTP Server
+******************************************************/
+#include "RancilioServer.h"
+std::vector<editable_t> editableVars = {
+    {"PID on?", kInteger, (void *)&pidON}, // ummm, why isn't pidON a boolean?
+    {"Start Kp", kDouble, (void *)&startKp},
+    {"Start Tn", kDouble, (void *)&startTn},
+    {"PID Kp", kDouble, (void *)&aggKp},
+    {"PID Tn", kDouble, (void *)&aggTn},
+    {"PID Tv", kDouble, (void *)&aggTv},
+    {"Set point", kDouble, (void *)&BrewSetPoint},
+    {"Brew Time", kDouble, (void *)&brewtime},
+};
 
 /********************************************************
   Get Wifi signal strength and set bars for display
@@ -1651,18 +1665,6 @@ void debugVerboseOutput()
   }
 }
 
-#ifdef ESP32
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
-#include <ESPAsyncWebServer.h>
-
-AsyncWebServer server(80);
-const char* PARAM_MESSAGE = "message";
-
 void setup() {
   DEBUGSTART(115200);
   debugStream.setup();
@@ -1815,37 +1817,9 @@ void setup() {
           displayLogo(langstring_connectwifi2[0], langstring_connectwifi2[1]);
         #endif
       }
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/plain", "Hello, world");
-    });
 
-    // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-        String message;
-        if (request->hasParam(PARAM_MESSAGE)) {
-            message = request->getParam(PARAM_MESSAGE)->value();
-        } else {
-            message = "No message sent";
-        }
-        request->send(200, "text/plain", "Hello, GET: " + message);
-    });
+      serverSetup();
 
-    // Send a POST request to <IP>/post with a form field message set to <message>
-    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
-        String message;
-        if (request->hasParam(PARAM_MESSAGE, true)) {
-            message = request->getParam(PARAM_MESSAGE, true)->value();
-        } else {
-            message = "No message sent";
-        }
-        request->send(200, "text/plain", "Hello, POST: " + message);
-    });
-
-    server.onNotFound([](AsyncWebServerRequest *request){
-        request->send(404, "text/plain", "Not found");
-    });
-
-    server.begin();
       delay(1000);
 
       //try blynk connection
