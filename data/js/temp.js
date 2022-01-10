27d895a4edac9779ca93d9e0d0a7ad6681c58cc8
Andreas Tacke <at@mail.fiendie.net>
2022-01-10 10:29:20 +0100

    Add range slidder to temperature plot

--- data/js/temp.js
+++ data/js/temp.js
@@ -14,7 +14,7 @@ var curTempTrace = {
   x: dates,
   y: curTempVals,
   line: {
-    color: '#008080', 
+    color: '#008080',
     shape: 'spline'
   }
 }
@@ -27,27 +27,49 @@ var targetTempTrace = {
   x: dates,
   y: targetTempVals,
   line: {
-    color: '#9932CC', 
+    color: '#9932CC',
     shape: 'spline'
   }
 }
 
-var data = [curTempTrace,targetTempTrace];
+var selectorOptions = {
+  buttons: [{
+    step: 'minute',
+    stepmode: 'backward',
+    count: 15,
+    label: '15m'
+  },
+  {
+    step: 'minute',
+    stepmode: 'backward',
+    count: 30,
+    label: '30m'
+  },
+  {
+    step: 'all',
+  }],
+};
+
+var data = [curTempTrace, targetTempTrace];
 
 var layout = {
   margin: {
-    l: 20,
-    r: 0,
+    l: 25,
+    r: 5,
     b: 20,
     t: 20,
     pad: 4
-  }, 
+  },
   showlegend: true,
-  legend:{
-    xanchor:"center",
-    yanchor:"top",
-    y:-0.2,
-    x:0.5
+  legend: {
+    xanchor: "center",
+    yanchor: "top",
+    y: -0.5,
+    x: 0.5
+  },
+  xaxis: {
+    rangeselector: selectorOptions,
+    rangeslider: {}
   },
   yaxis: {
     tickformat: '%H:%M:%S'
@@ -78,28 +100,32 @@ function plotTemperature(jsonValue) {
 
   var targetTemp = Number(jsonValue[targetTempKey]);
   console.log(targetTemp);
-  targetTempVals.push(targetTemp);  
-  
-  Plotly.extendTraces(chartDiv, {
-    x: [[dates[dates.length-1]], [dates[dates.length-1]]],
-    y: [[curTempVals[curTempVals.length-1]], [targetTempVals[targetTempVals.length-1]]]
-  }, [0, 1])
+  targetTempVals.push(targetTemp);
 
-  if(dates.length > 600) {
-      Plotly.relayout(chartDiv,
-        {
-          xaxis: {
-              range: [dates[dates.length-600], dates[dates.length-1]]
-          }
+  Plotly.extendTraces(
+    chartDiv,
+    {
+      x: [[dates[dates.length - 1]], [dates[dates.length - 1]]],
+      y: [[curTempVals[curTempVals.length - 1]], [targetTempVals[targetTempVals.length - 1]]]
+    },
+    [0, 1]
+  )
+
+  if (dates.length > 600) {
+    Plotly.relayout(chartDiv,
+      {
+        xaxis: {
+          range: [dates[dates.length - 600], dates[dates.length - 1]]
+        }
       });
   }
 }
 
 
-function getTemperatures(){
+function getTemperatures() {
   var xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var myObj = JSON.parse(this.responseText);
       console.log(myObj);
@@ -115,24 +141,39 @@ function getTemperatures(){
 if (!!window.EventSource) {
   var source = new EventSource('/events');
 
-  source.addEventListener('open', function(e) {
-    console.log("Event source connected");
-  }, false);
+  source.addEventListener(
+    'open',
+    function (e) {
+      console.log("Event source connected");
+    },
+    false
+  );
 
-  source.addEventListener('error', function(e) {
-    if (e.target.readyState != EventSource.OPEN) {
-      console.log("Events source disconnected");
-    }
-  }, false);
+  source.addEventListener(
+    'error',
+    function (e) {
+      if (e.target.readyState != EventSource.OPEN) {
+        console.log("Events source disconnected");
+      }
+    },
+    false
+  );
 
-  source.addEventListener('message', function(e) {
-    console.log("message", e.data);
-  }, false);
+  source.addEventListener(
+    'message',
+    function (e) {
+      console.log("message", e.data);
+    },
+    false);
 
-  source.addEventListener('new_temps', function(e) {
-    console.log("new_temps", e.data);
-    var myObj = JSON.parse(e.data);
-    console.log(myObj);
-    plotTemperature(myObj);
-  }, false);
+  source.addEventListener(
+    'new_temps',
+    function (e) {
+      console.log("new_temps", e.data);
+      var myObj = JSON.parse(e.data);
+      console.log(myObj);
+      plotTemperature(myObj);
+    },
+    false
+  );
 }
