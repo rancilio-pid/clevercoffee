b3b3831af9a02d4098383a71c6ced3cbe6061c7a
Miau09 <50794912+Miau09@users.noreply.github.com>
2022-02-06 15:04:00 +0100

    Update scalevoid.h
    fix compiler error if "DISPLAY 0" and "BREWMODE 2"

--- rancilio-pid/rancilio-pid/scalevoid.h
+++ rancilio-pid/rancilio-pid/scalevoid.h
@@ -34,24 +34,30 @@
     long stabilizingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilizing time
     boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
     DEBUG_print(F("INIT: Initializing scale ... "));
-    u8g2.clearBuffer();
-    u8g2.drawStr(0, 2, "Taring scale,");
-    u8g2.drawStr(0, 12, "remove any load!");
-    u8g2.drawStr(0, 22, "....");
-    delay(2000);
-    u8g2.sendBuffer();
+    #if (DISPLAY != 0)
+      u8g2.clearBuffer();
+      u8g2.drawStr(0, 2, "Taring scale,");
+      u8g2.drawStr(0, 12, "remove any load!");
+      u8g2.drawStr(0, 22, "....");
+      delay(2000);
+      u8g2.sendBuffer();
+    #endif    
     LoadCell.start(stabilizingtime, _tare);
     if (LoadCell.getTareTimeoutFlag()) {
       DEBUG_println(F("Timeout, check MCU>HX711 wiring and pin designations"));
-      u8g2.drawStr(0, 32, "failed!");
-      u8g2.drawStr(0, 42, "Scale not working...");    // scale timeout will most likely trigger after OTA update, but will still work after boot
-      delay(5000);
-      u8g2.sendBuffer();
+      #if (DISPLAY != 0)
+        u8g2.drawStr(0, 32, "failed!");
+        u8g2.drawStr(0, 42, "Scale not working...");    // scale timeout will most likely trigger after OTA update, but will still work after boot
+        delay(5000);
+        u8g2.sendBuffer();
+      #endif
     }
     else {
       DEBUG_println(F("done"));
-      u8g2.drawStr(0, 32, "done.");
-      u8g2.sendBuffer();
+      #if (DISPLAY != 0)
+        u8g2.drawStr(0, 32, "done.");
+        u8g2.sendBuffer();
+      #endif
     }
     LoadCell.setCalFactor(calibrationValue); // set calibration factor (float)
   }
