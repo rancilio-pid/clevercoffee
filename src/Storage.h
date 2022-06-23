daaf5659951a3bed08e599d26e936cae9b930bdf
JonasB2602 <107507181+JonasB2602@users.noreply.github.com>
2022-06-23 12:21:40 +0200

    Update Storage.h

--- src/Storage.h
+++ src/Storage.h
@@ -35,6 +35,9 @@ typedef enum
   STO_ITEM_WEIGHTSETPOINT,          // Brewweight setpoint
   STO_ITEM_PID_ON,                  // PID on/off state
   STO_ITEM_PID_KP_STEAM,            // PID P part at steam phase
+  STO_ITEM_PID_KP2_REGULAR          // PID P part at pressure profiling
+  STO_ITEM_PID_TN2_REGULAR          // PID I part at pressure profiling
+  STO_ITEM_PID_TV2_REGULAR          // PID D part at pressure profiling
 
   /* WHEN ADDING NEW ITEMS FOLLOWING HAS TO BE UPDATED:
    * - storage structure:  sto_data_t
