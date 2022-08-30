6fbb789664d2f5e546161b78c5c3b2812b8d51c4
Stefan Bethge <stefan+github@lanpartei.de>
2022-08-30 21:35:55 +0200

    include platformio.ini, remove _sample, update userConfig script

--- scripts/create_user_config.sh
+++ scripts/create_user_config.sh
@@ -18,22 +18,12 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         fi
 fi
 
-echo Please enter wireless network name and press Enter
-read WIFI_SSID
-
-echo Please enter wireless network password and press Enter
-read WIFI_PASSWORD
-
 echo Please enter Blynk authentification code
 read BLYNK_AUTH
 
 echo Please enter desired hostname
 read HOSTNAME
 
-
-${SED_COMMAND} -i "s/myssid/${WIFI_SSID}/" ${SOURCE_CODE_PATH}${USER_CONFIG_FILE_NAME}.h
-${SED_COMMAND} -i "s/mypass/${WIFI_PASSWORD}/" ${SOURCE_CODE_PATH}${USER_CONFIG_FILE_NAME}.h
 ${SED_COMMAND} -i "s/blynk_auth/${BLYNK_AUTH}/" ${SOURCE_CODE_PATH}${USER_CONFIG_FILE_NAME}.h
 ${SED_COMMAND} -i "s/ota_hostname/${HOSTNAME}/" ${SOURCE_CODE_PATH}${USER_CONFIG_FILE_NAME}.h
-${SED_COMMAND} -i "s/wifi-hostname/${HOSTNAME}/" ${SOURCE_CODE_PATH}${USER_CONFIG_FILE_NAME}.h
 
