61141ce6fbe73801ee3f8c8bf1a4c00b5060e9bb
Stefan Bethge <stefan+github@lanpartei.de>
2022-09-05 13:36:34 +0200

    don't use debugPrintf in Storage, __FUNCTION__ macro causes crash with it

--- src/RancilioServer.h
+++ src/RancilioServer.h
@@ -297,12 +297,11 @@ void serverSetup() {
     });
 
     server.on("/togglePid", HTTP_POST, [](AsyncWebServerRequest *request) {
-        debugPrintf("/togglepid requested, method: %d\n", request->method());
-
+        debugPrintf("/togglePid requested, method: %d\n", request->method());
         int status = flipUintValue(pidON);
 
         setPidStatus(status);
-        debugPrintf("Toggle PID state: %i \n", status);
+        debugPrintf("Toggle PID state: %d\n", status);
 
         request->redirect("/");
     });
