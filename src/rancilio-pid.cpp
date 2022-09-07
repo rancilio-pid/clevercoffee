aa98ad5755a8e9a00c88dfbcb9ab0bb91db8b035
Łukasz Piątkowski <piontec@gmail.com>
2022-09-07 23:06:00 +0200

    move SPI OLED mode pin definitions

--- src/rancilio-pid.cpp
+++ src/rancilio-pid.cpp
@@ -418,6 +418,9 @@ void getSignalStrength() {
 #if OLED_DISPLAY == 2
     U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);  // e.g. 0.96"
 #endif
+#if OLED_DISPLAY == 3
+    U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, OLED_CS, OLED_DC, /* reset=*/U8X8_PIN_NONE); // e.g. 1.3"
+#endif
 
 // Update for Display
 unsigned long previousMillisDisplay;  // initialisation at the end of init()
