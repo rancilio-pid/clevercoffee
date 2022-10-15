da767610a7cfe5c083d7d4782844ff3848794fa5
Alcoinus <felix.theiss@outlook.com>
2022-10-15 18:31:53 +0200

    Fix build for ESP32
    Compilation failed for ESP32, because "timer" reference to rancilio-pid.cpp had been undefined.

--- src/ISR.h
+++ src/ISR.h
@@ -13,6 +13,10 @@ extern unsigned int isrCounter;
 extern unsigned int windowSize;
 extern bool skipHeaterISR;
 
+#if defined(ESP32)
+extern hw_timer_t *timer;
+#endif
+
 void initTimer1(void);
 void enableTimer1(void);
 void disableTimer1(void);
