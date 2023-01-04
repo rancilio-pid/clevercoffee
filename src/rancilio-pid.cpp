993ab5e7cd289e210d553f0a938e99c468848dbc
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2023-01-04 19:35:32 +0100

    machinestate in mqtt

--- src/rancilio-pid.cpp
+++ src/rancilio-pid.cpp
@@ -2329,7 +2329,7 @@ void writeSysParamsToMQTT(void) {
         if (mqtt.connected() == 1) {
             // status topic (will sets it to offline)
             mqtt_publish("status", (char *)"online");
-
+            mqtt_publish("machinestate", (char *)machinestateEnumToString(machineState));
             mqtt_publish("temperature", number2string(temperature));
             mqtt_publish("brewSetPoint", number2string(brewSetPoint));
             mqtt_publish("brewTempOffset", number2string(brewTempOffset));
