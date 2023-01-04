5638cd618b4b7d5729ad0a940c879f97a58d5e43
Kilian Hohm <kilian.hohm@gmail.com>
2023-01-04 14:42:45 +0100

    Add machine state to mqtt topics

--- src/rancilio-pid.cpp
+++ src/rancilio-pid.cpp
@@ -2335,6 +2335,7 @@ void writeSysParamsToMQTT(void) {
         if (mqtt.connected() == 1) {
             // status topic (will sets it to offline)
             mqtt_publish("status", (char *)"online");
+            mqtt_publish("machineState", (char *)machinestateEnumToString(machineState));
 
             mqtt_publish("temperature", number2string(temperature));
             mqtt_publish("brewSetPoint", number2string(brewSetPoint));
