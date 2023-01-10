1a02b3f895e1e9fb77133dbdd17d7cd76e634d87
Michael <git@maunsen.de>
2023-01-10 16:54:55 +0100

    Fixed Timer Value to seconds, Added calibration function

--- src/rancilio-pid.cpp
+++ src/rancilio-pid.cpp
@@ -423,57 +423,6 @@ void getSignalStrength() {
     }
 }
 
-// Display define & template
-#if OLED_DISPLAY == 1
-    U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);  // e.g. 1.3"
-#endif
-
-#if OLED_DISPLAY == 2
-    U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);  // e.g. 0.96"
-#endif
-
-#if OLED_DISPLAY == 3
-    #define OLED_CS             5
-    #define OLED_DC             2
-    U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, OLED_CS, OLED_DC, /* reset=*/U8X8_PIN_NONE); // e.g. 1.3"
-#endif
-
-// Update for Display
-unsigned long previousMillisDisplay;  // initialisation at the end of init()
-const unsigned long intervalDisplay = 500;
-
-// Horizontal or vertical display
-#if (OLED_DISPLAY != 0)
-    #if (DISPLAYTEMPLATE < 20)  // horizontal templates
-        #include "display.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE >= 20)  // vertical templates
-        #include "Displayrotateupright.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE == 1)
-        #include "Displaytemplatestandard.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE == 2)
-        #include "Displaytemplateminimal.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE == 3)
-        #include "Displaytemplatetemponly.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE == 4)
-        #include "Displaytemplatescale.h"
-    #endif
-
-    #if (DISPLAYTEMPLATE == 20)
-        #include "Displaytemplateupright.h"
-    #endif
-#endif
-
-
 #if (PRESSURESENSOR == 1)  // Pressure sensor connected
     /**
      * Pressure sensor
@@ -675,6 +624,51 @@ boolean sys = readSysParamsFromStorage();
 // Display define & template
 #if OLED_DISPLAY == 1
     U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);  // e.g. 1.3"
+#endif
+
+#if OLED_DISPLAY == 2
+    U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);  // e.g. 0.96"
+#endif
+
+#if OLED_DISPLAY == 3
+    #define OLED_CS             5
+    #define OLED_DC             2
+    U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, OLED_CS, OLED_DC, /* reset=*/U8X8_PIN_NONE); // e.g. 1.3"
+#endif
+
+// Update for Display
+unsigned long previousMillisDisplay;  // initialisation at the end of init()
+const unsigned long intervalDisplay = 500;
+
+// Horizontal or vertical display
+#if (OLED_DISPLAY == 1 || OLED_DISPLAY == 2)
+    #if (DISPLAYTEMPLATE < 20)  // horizontal templates
+        #include "display.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE >= 20)  // vertical templates
+        #include "Displayrotateupright.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE == 1)
+        #include "Displaytemplatestandard.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE == 2)
+        #include "Displaytemplateminimal.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE == 3)
+        #include "Displaytemplatetemponly.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE == 4)
+        #include "Displaytemplatescale.h"
+    #endif
+
+    #if (DISPLAYTEMPLATE == 20)
+        #include "Displaytemplateupright.h"
+    #endif
 #endif
 
 #if OLED_DISPLAY == 2
