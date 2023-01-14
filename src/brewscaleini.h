4b635681e7e0fe721deae125adc1437856aa3319
Michael Maune <maunsen@gmail.com>
2023-01-14 13:27:02 +0100

    added brewscale ini

--- src/brewscaleini.h
+++ src/brewscaleini.h
@@ -33,11 +33,10 @@ unsigned long previousMillistempanalogreading;      // ms for analogreading
 // Shot timer with or without scale
 #if (ONLYPIDSCALE == 1 || BREWMODE == 2)
     int shottimercounter = 10 ;
-    
     float weight = 0;                                   // value from HX711
     float weightPreBrew = 0;                            // value of scale before wrew started
     float weightBrew = 0;                               // weight value of brew
-    float scaleDelayValue = 0.5;                        // value in gramm that takes still flows onto the scale after brew is stopped
+    float scaleDelayValue = 2.5;                        // value in gramm that takes still flows onto the scale after brew is stopped
     bool scaleFailure = false;
     const unsigned long intervalWeight = 200;           // weight scale
     unsigned long previousMillisScale;                  // initialisation at the end of init()
