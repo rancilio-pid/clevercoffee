0009ecd5067de7955dab2a55e795fadd5fb1424f
Michael Maune <maunsen@gmail.com>
2023-01-19 12:24:19 +0100

    removed empty lines

--- src/brewscaleini.h
+++ src/brewscaleini.h
@@ -40,8 +40,6 @@ boolean brewPIDdisabled = false;                    // is PID disabled for delay
 const unsigned long analogreadingtimeinterval = 10; // ms
 unsigned long previousMillistempanalogreading;      // ms for analogreading
 
-
-
 // Shot timer with or without scale
 #if (ONLYPIDSCALE == 1 || BREWMODE == 2)
     int shottimercounter = 10 ;
