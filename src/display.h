429fa11a22c7f5b03f51f4be5c95a7afcfb3deac
LoQue90 <98092139+LoQue90@users.noreply.github.com>
2023-01-23 23:42:00 +0100

    Move MQTT Indicator
    Move MQTT Icon in all display versions closer to Wifi Signal Bars

--- src/display.h
+++ src/display.h
@@ -181,11 +181,11 @@ void Displaymachinestate() {
 
             if (MQTT == 1) {
                 if (mqtt.connected() == 1) {
-                    u8g2.setCursor(77, 1);
+                    u8g2.setCursor(60, 1);
                     u8g2.setFont(u8g2_font_profont11_tf);
                     u8g2.print("MQTT");
                 } else {
-                    u8g2.setCursor(77, 2);
+                    u8g2.setCursor(60, 2);
                     u8g2.print("");
                 }
             }
