834cc6da89c3ac377106848e3d79e5cc08edc804
Michael Maune <maunsen@gmail.com>
2023-01-25 09:47:49 +0100

    calibration

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -62,9 +62,7 @@ typedef struct __attribute__((packed)) {
     double scaleKnownWeight;
     double steamSetpoint;
     float scaleCalibration;
-    double scaleCalibration;
     double scaleKnownWeight;
-    double steamSetpoint;
 } sto_data_t;
 
 // set item defaults
