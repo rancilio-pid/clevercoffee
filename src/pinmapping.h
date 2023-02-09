c308d40ef11cb72ca30f04f32f26de57c518dcef
Michael Maune <maunsen@gmail.com>
2023-02-09 09:01:26 +0100

    reverted local changes in platform.ini and pinmapping to standard

--- src/pinmapping.h
+++ src/pinmapping.h
@@ -12,21 +12,21 @@
  */
 
 // Switches/Buttons
-#define PIN_POWERSWITCH 99
-#define PIN_BREWSWITCH 16
-#define PIN_STEAMSWITCH 17
+#define PIN_POWERSWITCH 39
+#define PIN_BREWSWITCH 34
+#define PIN_STEAMSWITCH 35
 
 #define PIN_ROTARY_DT 3         // Rotary encoder data pin
 #define PIN_ROTARY_CLK 4        // Rotary encoder clock pin
-#define PIN_ROTARY_SW 5        // Rotary encoder switch
+#define PIN_ROTARY_SW 5         // Rotary encoder switch
 
 // Sensors
-#define PIN_TEMPSENSOR 2
-#define PIN_PRESSURESENSOR 99
-#define PIN_WATERSENSOR 99
-#define PIN_FLOWSENSOR 99
-#define PIN_HXDAT 22            // Brew scale data pin
-#define PIN_HXCLK 23            // Brew scale clock pin
+#define PIN_TEMPSENSOR 16
+#define PIN_PRESSURESENSOR 23
+#define PIN_WATERSENSOR 36
+#define PIN_FLOWSENSOR 1
+#define PIN_HXDAT 32            // Brew scale data pin
+#define PIN_HXCLK 33            // Brew scale clock pin
 
 
 /**
@@ -34,20 +34,20 @@
  */
 
 // Relays
-#define PIN_VALVE 12
-#define PIN_PUMP 14
-#define PIN_HEATER 25
+#define PIN_VALVE 17
+#define PIN_PUMP 27
+#define PIN_HEATER 2
 
 // LEDs
-#define PIN_STATUSLED 99
+#define PIN_STATUSLED 26
 
 // Periphery
-#define PIN_ZC 99               // Dimmer circuit Zero Crossing
-#define PIN_PSM 99              // Dimmer circuit PSM
+#define PIN_ZC 18               // Dimmer circuit Zero Crossing
+#define PIN_PSM 19              // Dimmer circuit PSM
 
 
 /**
  * Bidirectional Pins
  */
-#define PIN_I2CSCL 18
-#define PIN_I2CSDA 19
+#define PIN_I2CSCL 22
+#define PIN_I2CSDA 21
