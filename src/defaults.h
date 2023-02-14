450e0cf04ad48e0aff3f4e507a63641dccf3811a
LoQue90 <98092139+LoQue90@users.noreply.github.com>
2023-02-14 08:37:45 +0100

    fix misspelling, adding min/max values
    Co-Authored-By: Stefan Bethge <kjyv@users.noreply.github.com>

--- src/defaults.h
+++ src/defaults.h
@@ -41,7 +41,7 @@ int writeSysParamsToStorage(void);
 #define PRE_INFUSION_PAUSE_TIME 5  // pre-infusion pause time in seconds
 #define SCALE_WEIGHTSETPOINT 30    // Target weight in grams
 #define WIFI_CREDENTIALS_SAVED 0   // Flag if wifi setup is done. 0: not set up, 1: credentials set up via wifi manager
-#define STEAMSWITCHTYP 0           // Steamswitch 0 = disable, 1 = normal switch 2 = trigger
+#define STEAMSWITCHTYPE 0           // Steamswitch 0 = disable, 1 = normal switch 2 = trigger
 
 // Backflush values
 #define FILLTIME 3000              // time in ms the pump is running
@@ -70,6 +70,8 @@ int writeSysParamsToStorage(void);
 #define BREW_SETPOINT_MAX 110
 #define STEAM_SETPOINT_MIN 100
 #define STEAM_SETPOINT_MAX 140
+#define STEAM_SWITCHTYPE_MIN 0
+#define STEAM_SWITCHTYPE_MAX 2
 #define BREW_TEMP_OFFSET_MIN 0
 #define BREW_TEMP_OFFSET_MAX 20
 #define BREW_TEMP_TIME_MIN 1
