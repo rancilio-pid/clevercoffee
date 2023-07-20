ef366ad2b70a58dfbda9bd8972bcb544d4e9e6dd
SphaeroX <SphaeroX@gmail.com>
2023-07-20 13:08:41 +0200

    integrity hash was wrong, bs not loaded. removed for local

--- src/EmbeddedWebserver.h
+++ src/EmbeddedWebserver.h
@@ -175,7 +175,7 @@ String getHeader(String varName) {
             #endif
         case (str2int("BOOTSTRAP_BUNDLE")):
             #if NOINTERNET == 1
-                return F("<script src=\"/js/bootstrap.bundle.5.2.3.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\"></script>");
+                return F("<script src=\"/js/bootstrap.bundle.5.2.3.min.js\"></script>");
             #else
                 return F("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>");
             #endif
