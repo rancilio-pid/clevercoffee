d2116c58147a9054f413f9a658e5f3b0eb73c4d6
Simon Spannagel <simonspa@kth.se>
2024-04-15 00:07:24 +0200

    Reduce Timer Verbosity

--- src/utils/Timer.cpp
+++ src/utils/Timer.cpp
@@ -7,7 +7,7 @@ void Timer::operator()() {
     auto current = millis();
 
     if ((current - last_) >= interval_) {
-        LOG(DEBUG, "Timer expired, calling function");
+        LOG(TRACE, "Timer expired, calling function");
 
         // Update timer
         last_ = current;
