bc324ea83011313ea07984a9403e537901959941
Simon Spannagel <simonspa@kth.se>
2024-04-21 19:51:41 +0200

    TempSensor: move error checking & timer into its class

--- src/hardware/TempSensor.h
+++ src/hardware/TempSensor.h
@@ -11,35 +11,36 @@
 #include <numeric>
 
 #include "Logger.h"
+#include "utils/Timer.h"
 
 class TempSensor {
     public:
+        /**
+         * @brief Abstract temperature sensor class
+         * @details This class controls a temperature sensor. It updates the value in regular intervals of 400ms, controlled
+         *          by a timer. The temperature sampling result is checked for errors and an internal error counter is kept
+         *          to detect consecutive reading errors
+         */
+        TempSensor() :
+            update_temperature(std::bind(&TempSensor::update_temperature_reading, this), 400) {
+        }
+
         /**
          * @brief Returns the current temperature
          * @details Requests sampling from attached sensor and returns reading.
          * @return Temperature in degrees Celsius
          */
-        virtual float getCurrentTemperature() {
-            auto temperature = sample_temperature();
-            last_temperature_ = temperature;
-            update_moving_average();
-            return temperature;
+        double getCurrentTemperature() {
+            // Trigger the timer to update the temperature:
+            update_temperature();
+            return last_temperature_;
         }
 
-        float getAverageTemperatureRate() {
+        double getAverageTemperatureRate() {
+            // Trigger the timer to update the temperature:
+            update_temperature();
             return average_temp_rate_;
         }
-
-        /**
-         * @brief Returns sampling interval for the sensor
-         * @details Returns a value to control how often the sensor is sampled. This is a static value, typically fixed for
-         *          a given sensor hardware type and is used to avoid polling the sensor too often without new readings
-         *          available.
-         * @return Sampling interval in milliseconds
-         */
-        virtual int getSamplingInterval() const {
-            return 400;
-        };
 
         /**
          * @brief Default destructor
@@ -47,36 +48,9 @@ class TempSensor {
         virtual ~TempSensor() = default;
 
         /**
-         * @brief check sensor value.
-         * @return If < 0 or difference between old and new >25, then increase error.
-         *      If error is equal to maxErrorCounter, then set sensorError
+         * @brief Returns error state of the temperature sensor
+         * @return true if the sensor is in error, false otherwise
          */
-        bool check(float temperature, float offset) {
-            bool sensorOK = false;
-            bool badCondition = (temperature < 0 || temperature > 150 || std::fabs(temperature - last_temperature_) > (5 + offset));
-
-            if (badCondition && !error_) {
-                bad_readings_++;
-                sensorOK = false;
-
-                LOGF(WARNING, "temperature sensor reading: consec_errors = %i, temp_current = %.1f, temp_prev = %.1f", bad_readings_, temperature, last_temperature_);
-            }
-            else if (badCondition == false && sensorOK == false) {
-                bad_readings_ = 0;
-                sensorOK = true;
-            }
-
-            if (bad_readings_ >= max_bad_treadings_ && !error_) {
-                error_ = true;
-                LOGF(ERROR, "temperature sensor malfunction: temp_current = %.1f", temperature);
-            }
-            else if (bad_readings_ == 0 && error_) {
-                error_ = false;
-            }
-
-            return sensorOK;
-        }
-
         bool hasError() {
             return error_;
         }
@@ -85,12 +59,44 @@ class TempSensor {
         /**
          * @brief Samples the current temperature from the sensor
          * @details Requests sampling from attached sensor and returns reading. This method is purely virtual and must be
-         *          implemented by every child class.
-         * @return Temperature in degrees Celsius
+         *          implemented by every child class. The argument is passed by reference and is updated if the return value
+         *          of the function is true.
+         * @return Boolean indicating whether the reading has been successful
          */
         virtual bool sample_temperature(double& temperature) const = 0;
 
     private:
+        /**
+         * @brief Small helper method to be wrapped in a timer for updating the temperature from the sensors
+         */
+        void update_temperature_reading() {
+            // Update temperature and detect errors:
+            auto updated = sample_temperature(last_temperature_);
+            if (updated) {
+                // Reset error counter and error state
+                bad_readings_ = 0;
+                error_ = false;
+
+                // Update moving average
+                update_moving_average();
+            }
+            else if (!error_) {
+                // Increment error counter
+                bad_readings_++;
+            }
+
+            if (bad_readings_ >= max_bad_treadings_ && !error_) {
+                error_ = true;
+                LOGF(ERROR, "Temperature sensor malfunction, %i consecutive errors", bad_readings_);
+            }
+        }
+
+        Timer update_temperature;
+        double last_temperature_{};
+        int bad_readings_{0};
+        int max_bad_treadings_{10};
+        bool error_{false};
+
         /**
          * @brief FIR moving average filter for software brew detection
          */
@@ -130,15 +136,8 @@ class TempSensor {
             }
         }
 
-        float last_temperature_{};
-        float average_temp_rate_{};
-
-        int bad_readings_{0};
-        int max_bad_treadings_{10};
-
-        bool error_{false};
-
         // Moving average:
+        double average_temp_rate_{};
         constexpr static size_t numValues = 15;
         std::array<double, numValues> tempValues{};        // array of temp values
         std::array<unsigned long, numValues> timeValues{}; // array of time values
