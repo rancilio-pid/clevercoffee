7ca23039ca1cee310864553f7fd675b2fe0bc122
Simon Spannagel <simonspa@kth.se>
2024-04-21 19:36:16 +0200

    TempSensor: Dallas knows error codes - let's use them!

--- src/hardware/TempSensorDallas.cpp
+++ src/hardware/TempSensorDallas.cpp
@@ -12,9 +12,32 @@ TempSensorDallas::TempSensorDallas(int GPIOPin) {
     dallasSensor_->begin();
     dallasSensor_->getAddress(sensorDeviceAddress_, 0);
     dallasSensor_->setResolution(sensorDeviceAddress_, 10);
+
+    // Request first temperature conversion directly:
+    dallasSensor_->requestTemperaturesByAddress(sensorDeviceAddress_);
 }
 
-float TempSensorDallas::sample_temperature() const {
-    dallasSensor_->requestTemperatures();
-    return dallasSensor_->getTempCByIndex(0);
+bool TempSensorDallas::sample_temperature(double& temperature) const {
+
+    // Read temperature from device
+    auto temp = dallasSensor_->getTempC(sensorDeviceAddress_);
+
+    // Check error codes:
+    if (temp == DEVICE_DISCONNECTED_C) {
+        LOG(WARNING, "Temperature sensor not connected");
+        return false;
+    }
+    else if (temp == DEVICE_FAULT_OPEN_C || temp == DEVICE_FAULT_SHORTGND_C || temp == DEVICE_FAULT_SHORTVDD_C) {
+        LOG(WARNING, "Issue with temperature sensor connection, check wiring");
+        return false;
+    }
+
+    // All fine, return temp:
+    temperature = temp;
+
+    // Request next temperature conversion from device, to be read the next time
+    // For 10-bit resolution the conversion takes around 188ms and our temperature reading timer clocks at 400ms - checks out!
+    dallasSensor_->requestTemperaturesByAddress(sensorDeviceAddress_);
+
+    return true;
 }
