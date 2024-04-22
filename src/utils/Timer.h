0181d0ac1c2d0a60637d7bdf246e697934138a60
Simon Spannagel <simonspa@kth.se>
2024-04-22 06:34:54 +0200

    Improve timer class

--- src/utils/Timer.h
+++ src/utils/Timer.h
@@ -6,14 +6,34 @@
 
 #pragma once
 
+#include "Arduino.h"
+
 #include <functional>
 
 class Timer {
     public:
+        /** No default constructor */
         Timer() = delete;
-        Timer(std::function<void()> func, unsigned long interval) :
-            callback_(func), interval_(interval) {};
+
+        /**
+         * @brief COnstructor taking a function pointer and the desired interval
+         *
+         * @param func Function pointer for the callback to be executed upon timer expiry
+         * @param long Desired interval between calls in milliseconds
+         */
+        Timer(std::function<void()> func, unsigned long interval);
+
+        /**
+         * @brief Call operator for timer invocation
+         * @details This will check if the timer expired and invoke the callback
+         */
         void operator()();
+
+        /**
+         * @brief Reset the timer
+         * @details This resets the timer such that the callback will be executed directly upon its next invocation
+         */
+        void reset();
 
     private:
         // Callback to be executed when timer runs out:
@@ -22,6 +42,6 @@ class Timer {
         // Interval of timer execution
         unsigned long interval_;
 
-        // Last time the timer ran out
-        unsigned long last_{0};
+        // Next time the timer will run out
+        unsigned long next_;
 };
