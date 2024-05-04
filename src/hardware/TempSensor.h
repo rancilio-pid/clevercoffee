5982d0c8c2d63d44531969fc563f775ea78d60f4
Simon Spannagel <simonspa@kth.se>
2024-05-04 09:26:18 +0200

    Improve logging of TempSensor

--- src/hardware/TempSensor.h
+++ src/hardware/TempSensor.h
@@ -70,11 +70,10 @@ class TempSensor {
          * @brief Small helper method to be wrapped in a timer for updating the temperature from the sensors
          */
         void update_temperature_reading() {
-            LOG(TRACE, "Attempting temperature reading");
             // Update temperature and detect errors:
             auto updated = sample_temperature(last_temperature_);
             if (updated) {
-                LOGF(DEBUG, "Temperature reading successful: %.1f", last_temperature_);
+                LOGF(TRACE, "Temperature reading successful: %.1f", last_temperature_);
 
                 // Reset error counter and error state
                 bad_readings_ = 0;
@@ -85,6 +84,7 @@ class TempSensor {
             }
             else if (!error_) {
                 // Increment error counter
+                LOGF(DEBUG, "Error during temperature reading, incrementing error counter to %i", bad_readings_);
                 bad_readings_++;
             }
 
