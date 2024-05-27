ca94696d3c106a2e2b2331712db83125c957abf4
Thorsten <git@thorsten-liebich.de>
2024-05-28 00:12:36 +0200

    deleted waterOn functionality outside of waterHandler.h

--- src/main.cpp
+++ src/main.cpp
@@ -1925,14 +1925,6 @@ void looppid() {
         setpoint = brewSetpoint;
     }
 
-    // TL
-    /*if (waterON && machineState == kWater) {
-        pumpRelay.on();
-    }
-    else if (!waterON && machineState == kWater) {
-        pumpRelay.off();
-    }*/
-
     updateStandbyTimer();
 
     handleMachineState();
